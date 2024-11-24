255ac6f32987784357e12ef6ff8c0f4c3b29859c
Naoki.Yasuno <Naoki.Yasuno@sony.com>
2024-11-24 18:11:55 +0900

    Add --move_num to ncs_split.py

--- src/ncs_split.py
+++ src/ncs_split.py
@@ -12,7 +12,7 @@ NOTATION_AREA_RIGHT = 1280
 MOVE_ID_WIDTH = 50
 
 
-def split_sheet_image_into_move(sheet_image_path, out_dir_path):
+def split_sheet_image_into_move(sheet_image_path, out_dir_path, move_num):
     # load data
     sheet_image = cv2.imread(sheet_image_path, cv2.IMREAD_GRAYSCALE)
     if sheet_image is None:
@@ -69,14 +69,17 @@ def split_sheet_image_into_move(sheet_image_path, out_dir_path):
     # Split and output images for each move
     os.makedirs(out_dir_path, exist_ok=True)
     move_image_path_list = []
-    # FIXME limit upto args.move_num
     for move_idx in range(move_num_on_sheet * 2):
+        if move_idx >= move_num:
+            break
+
         x1, y1, x2, y2 = move_rect_list[move_idx]
         img_cropped = sheet_image[y1:y2, x1:x2]
 
         # for make it easy to sort, add 0, 1 prefix
         color = "0white" if move_idx % 2 == 0 else "1black"
-        move_name = str((move_idx // 2) + 1) + "_" + color
+        move_name_idx = (move_idx // 2) + 1
+        move_name = "{:0=4}".format(move_name_idx) + "_" + color
         move_image_path = os.path.join(out_dir_path, move_name + '.png')
         move_image_path_list.append(move_image_path)
         cv2.imwrite(move_image_path, img_cropped)
@@ -85,15 +88,14 @@ def split_sheet_image_into_move(sheet_image_path, out_dir_path):
 
 
 def main(args):
-    split_sheet_image_into_move(args.input, args.output)
+    split_sheet_image_into_move(args.input, args.output, args.move_num)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', '-i', default="data/kanagawa_champ_2023/images/1.jpeg")
     parser.add_argument('--output', default="data/kanagawa_champ_2023/images_move/1/")
-    # TODO: add move_num to limit output
-    # parser.add_argument('--move_num', default=60)
+    parser.add_argument('--move_num', type=int, default=60)
     args = parser.parse_args()
 
     main(args)
