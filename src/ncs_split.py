81c2c276930abd141abbb5ce30b65a71edc5d63f
Naoki.Yasuno <Naoki.Yasuno@sony.com>
2024-11-24 18:02:54 +0900

    Change ncs_split.py input: dir -> image path

--- src/ncs_split.py
+++ src/ncs_split.py
@@ -1,12 +1,9 @@
 import argparse
-from pprint import pprint
 import cv2
-import pdb
 import os
-import matplotlib.pyplot as plt
-import time
 
 
+# These parameter is for NCS score sheet
 NOTATION_AREA_UPPER = 260
 NOTATION_AREA_LOWER = 1800
 NOTATION_AREA_LEFT = 150
@@ -15,36 +12,12 @@ NOTATION_AREA_RIGHT = 1280
 MOVE_ID_WIDTH = 50
 
 
-def get_data_sheet(args):
-    """
-    Load all the data
-    Args:
-        args: list of image path (1 path = 1 sheet), list of labels (1 label = 1 sheet)
-              (label one or multple rows, and 1 row = 1 move:w)
-    """
-    training_tags = open(f"{args.input}/training_tags.txt", "r").readlines()
-
-    ret = {}
-    for line in training_tags:
-        line = line.strip()
-        game_id, sheet_id, move_id, color = line.split('_')
-        color = color.split(" ")[0].strip(".png")
-        move = line.split(' ')[-1]
-        if game_id not in ret.keys():
-            ret[game_id] = {}
-        if sheet_id not in ret[game_id].keys():
-            ret[game_id][sheet_id] = {}
-        move_name = move_id + "_" + color
-        ret[game_id][sheet_id][move_name] = move
-
-    return ret
-
-
-def split_sheet_image_into_move(sheet_image_path):
+def split_sheet_image_into_move(sheet_image_path, out_dir_path):
     # load data
     sheet_image = cv2.imread(sheet_image_path, cv2.IMREAD_GRAYSCALE)
     if sheet_image is None:
-        return []
+        print("Error: sheet_image is None")
+        return
 
     move_rect_list = []  # (x1, y1, x2, y2)
     img_height, img_width = sheet_image.shape
@@ -94,18 +67,17 @@ def split_sheet_image_into_move(sheet_image_path):
             move_rect_list.append((x1, y1, x2, y2))
 
     # Split and output images for each move
-    move_image_dir = sheet_image_path + '_moves/'
-    os.makedirs(move_image_dir, exist_ok=True)
-    sheet_image_path = move_image_dir + ''
+    os.makedirs(out_dir_path, exist_ok=True)
     move_image_path_list = []
-    # FIXME limit upto the max move ID
+    # FIXME limit upto args.move_num
     for move_idx in range(move_num_on_sheet * 2):
         x1, y1, x2, y2 = move_rect_list[move_idx]
         img_cropped = sheet_image[y1:y2, x1:x2]
 
-        color = "white" if move_idx % 2 == 0 else "black"
+        # for make it easy to sort, add 0, 1 prefix
+        color = "0white" if move_idx % 2 == 0 else "1black"
         move_name = str((move_idx // 2) + 1) + "_" + color
-        move_image_path = move_image_dir + '/' + move_name + '.png'
+        move_image_path = os.path.join(out_dir_path, move_name + '.png')
         move_image_path_list.append(move_image_path)
         cv2.imwrite(move_image_path, img_cropped)
 
@@ -113,15 +85,15 @@ def split_sheet_image_into_move(sheet_image_path):
 
 
 def main(args):
-    image_path_list = os.listdir(args.input)
-    for image_path in image_path_list:
-        split_sheet_image_into_move(args.input + '/' + image_path)
+    split_sheet_image_into_move(args.input, args.output)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', '-i', default="data/kanagawa_champ_2023/images")
-    # parser.add_argument('--output', default="move_dir/")
+    parser.add_argument('--input', '-i', default="data/kanagawa_champ_2023/images/1.jpeg")
+    parser.add_argument('--output', default="data/kanagawa_champ_2023/images_move/1/")
+    # TODO: add move_num to limit output
+    # parser.add_argument('--move_num', default=60)
     args = parser.parse_args()
 
     main(args)
